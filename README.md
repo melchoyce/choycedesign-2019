7a9a666d940c70f987406375858e00caac7d4c47
Kelly Dwan <ryelle@users.noreply.github.com>
2018-08-28 23:09:44 -0400

    Initial commit of starter theme

---
+++ README.md
@@ -0,0 +1,2 @@
+melchoyce.design
+================
---
+++ inc/jetpack.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ *
+ * @link https://jetpack.com/
+ *
+ * @package Choyce Design
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ * See: https://jetpack.com/support/content-options/
+ */
+function choycedesign_2018_jetpack_setup() {
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+
+	// Add theme support for Content Options.
+	add_theme_support( 'jetpack-content-options', array(
+		'post-details' => array(
+			'stylesheet' => 'choycedesign-2018-style',
+			'date'       => '.posted-on',
+			'categories' => '.cat-links',
+			'tags'       => '.tags-links',
+			'author'     => '.byline',
+			'comment'    => '.comments-link',
+		),
+	) );
+}
+add_action( 'after_setup_theme', 'choycedesign_2018_jetpack_setup' );
