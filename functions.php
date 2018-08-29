9580f2fb2008a6ee5381fcf52916031ce827443b
Kelly Dwan <ryelle@users.noreply.github.com>
2018-08-29 00:29:42 -0400

    Remove deleted file

--- functions.php
+++ functions.php
@@ -167,11 +167,6 @@ function choycedesign_2018_scripts() {
 add_action( 'wp_enqueue_scripts', 'choycedesign_2018_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
