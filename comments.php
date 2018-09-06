4bf2d62a4072e51b18d691a22bd91eeb9d4be6c9
Kelly Dwan <ryelle@users.noreply.github.com>
2018-09-06 19:03:13 -0400

    Style pingbacks

--- comments.php
+++ comments.php
@@ -23,44 +23,63 @@ if ( post_password_required() ) {
 <div id="comments" class="comments-area">
 
 	<?php
-	// You can start editing here -- including this comment!
+	global $wp_query;
+	$comments_by_type = separate_comments( $wp_query->comments );
+
 	if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-			$comment_count = get_comments_number();
-			if ( 1 === $comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', 'choycedesign' ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			} else {
-				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'choycedesign' ) ),
-					number_format_i18n( $comment_count ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			}
-			?>
-		</h2><!-- .comments-title -->
 
-		<?php the_comments_navigation(); ?>
+		<?php if ( count( $comments_by_type['comment'] ) ) : ?>
+			<h2 class="comments-title">
+				<?php
+				$comment_count = count( $comments_by_type['comment'] );
+				if ( 1 === $comment_count ) {
+					printf(
+						/* translators: 1: title. */
+						esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', 'choycedesign' ),
+						'<span>' . get_the_title() . '</span>'
+					);
+				} else {
+					printf( // WPCS: XSS OK.
+						/* translators: 1: comment count number, 2: title. */
+						esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'choycedesign' ) ),
+						number_format_i18n( $comment_count ),
+						'<span>' . get_the_title() . '</span>'
+					);
+				}
+				?>
+			</h2><!-- .comments-title -->
 
-		<ol class="comment-list">
-			<?php
-				wp_list_comments( array(
-					'callback'    => 'choycedesign_2018_comments',
-					'avatar_size' => 128,
-					'style'       => 'ol',
-					'short_ping'  => true,
-				) );
-			?>
-		</ol><!-- .comment-list -->
+			<?php the_comments_navigation(); ?>
 
-		<?php the_comments_navigation();
+			<ol class="comment-list">
+				<?php
+					wp_list_comments( array(
+						'callback'    => 'choycedesign_2018_comments',
+						'avatar_size' => 128,
+						'style'       => 'ol',
+						'type'        => 'comment',
+					) );
+				?>
+			</ol><!-- .comment-list -->
 
-		// If comments are closed and there are comments, let's leave a little note, shall we?
+			<?php the_comments_navigation(); ?>
+		<?php endif; ?>
+
+		<?php if ( count( $comments_by_type['pings'] ) ) : ?>
+			<h2 class="pings-title"><?php _e( 'Mentions', 'choycedesign' ); ?></h2>
+
+			<ul class="pings-list">
+				<?php
+					wp_list_comments( array(
+						'callback'    => 'choycedesign_2018_pings',
+						'style'       => 'ul',
+						'type'        => 'pings',
+					) );
+				?>
+			</ul><!-- .pings-list -->
+		<?php endif; ?>
+
+		<?php
 		if ( ! comments_open() ) : ?>
 			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'choycedesign' ); ?></p>
 		<?php
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -10,7 +10,7 @@
 /**
  * Prints HTML with meta information for the current post-date/time and author.
  */
-function choycedesign_2018_posted_on() {
+function choycedesign_2018_entry_footer() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
@@ -30,52 +30,6 @@ function choycedesign_2018_posted_on() {
 	);
 
 	echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
-}
-
-/**
- * Prints HTML with meta information for the categories, tags and comments.
- */
-function choycedesign_2018_entry_footer() {
-	// Hide category and tag text for pages.
-	if ( 'post' === get_post_type() ) {
-		choycedesign_2018_posted_on();
-	}
-
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		comments_popup_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: post title */
-					__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'choycedesign' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			)
-		);
-		echo '</span>';
-	}
-
-	edit_post_link(
-		sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Edit <span class="screen-reader-text">%s</span>', 'choycedesign' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		),
-		'<span class="edit-link">',
-		'</span>'
-	);
 }
 
 /**
@@ -125,5 +79,15 @@ function choycedesign_2018_comments( $comment, $args, $depth ) {
 				) ) );
 			?>
 		</article><!-- .comment-body -->
+	<?php
+}
+
+/**
+ * Format the pings.
+ */
+function choycedesign_2018_pings( $comment, $args, $depth ) {
+	?>
+	<li id="comment-<?php comment_ID(); ?>" <?php comment_class( '', $comment ); ?>>
+		<?php comment_author_link( $comment ); ?>
 	<?php
 }
