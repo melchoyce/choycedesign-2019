9802adb71f10cb676e7140686b5899711a1b3863
Kelly Dwan <ryelle@users.noreply.github.com>
2018-09-09 12:47:23 -0400

    Move posts list caching into separate functions, update transient on post-publish

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -47,3 +47,65 @@ function choycedesign_2018_post_navigation( $template, $class ) {
 	return str_replace( '</h2>', '</h2>' . $all_posts, $template );
 }
 add_filter( 'navigation_markup_template', 'choycedesign_2018_post_navigation', 10, 2 );
+
+/**
+ * Get a list of all posts, sorted by year, from cached value. Rebuild cache if not found.
+ */
+function choycedesign_2018_get_posts_by_year() {
+	$all_posts_by_year = get_transient( 'cd2018_archives_by_year' );
+	if ( false === $all_posts_by_year ) {
+		$all_posts_by_year = _choycedesign_2018_generate_posts();
+		set_transient( 'cd2018_archives_by_year', $all_posts_by_year, WEEK_IN_SECONDS );
+	}
+
+	return $all_posts_by_year;
+}
+
+/**
+ * Get a list of all posts, sorted by year (this function bypasses cache).
+ */
+function _choycedesign_2018_generate_posts() {
+	$all_posts = get_posts( array(
+		'posts_per_page' => -1,
+		'post_type' => 'post',
+		'suppress_filters' => true,
+		// TODO Only posts in the last 3? 4? years (or last 40-50?)
+	) );
+
+	$all_posts_by_year = array();
+
+	foreach( $all_posts as $post ) {
+		$year = mysql2date( 'Y', $post->post_date );
+		$link = sprintf(
+			'<a href="%s" rel="bookmark">%s</a>',
+			esc_url( get_permalink( $post ) ),
+			esc_html( get_the_title( $post ) )
+		);
+
+		if ( ! isset( $all_posts_by_year ) ) {
+			$all_posts_by_year[ $year ] = [ $link ];
+		} else {
+			$all_posts_by_year[ $year ][] = $link;
+		}
+	}
+
+	krsort( $all_posts_by_year );
+
+	return $all_posts_by_year;
+}
+
+/**
+ * Flush the transient when posts are added/deleted.
+ */
+function choycedesign_2018_update_post_cache( $new_status, $old_status, $post ) {
+	if ( 'post' !== $post->post_type ) {
+		return;
+	}
+	// If newly published or newly deleted…
+	if ( ( 'publish' === $new_status && 'publish' !== $old_status )
+		|| ( 'trash' === $new_status && 'trash' !== $old_status ) ) {
+		$all_posts_by_year = _choycedesign_2018_generate_posts();
+		set_transient( 'cd2018_archives_by_year', $all_posts_by_year, WEEK_IN_SECONDS );
+	}
+}
+add_action( 'transition_post_status', 'choycedesign_2018_update_post_cache', 10, 3 );
--- template-parts/archive.php
+++ template-parts/archive.php
@@ -5,45 +5,18 @@
  * @package Choyce Design
  */
 
-// TODO Cache the sorted list
-$all_posts = get_posts( array(
-	'posts_per_page' => -1,
-	'post_type' => 'post',
-	'suppress_filters' => true,
-	// TODO Only posts in the last 3? 4? years
-) );
+$all_posts_by_year = choycedesign_2018_get_posts_by_year();
 
-$all_posts_by_year = [];
-
-foreach( $all_posts as $post ) {
-	$year = mysql2date( 'Y', $post->post_date );
-	if ( ! isset( $all_posts_by_year ) ) {
-		$all_posts_by_year[ $year ] = [ $post ];
-	} else {
-		$all_posts_by_year[ $year ][] = $post;
-	}
-}
-
-ksort( $all_posts );
-
-// END CACHED FUNCTION… eventually
-
-foreach( $all_posts_by_year as $year => $posts ) {
+foreach( $all_posts_by_year as $year => $links ) {
 ?>
 	<section class="archive">
 		<h2 class="archive-year"><?php echo $year; ?></h2>
 
 		<ul class="archive-list">
-		<?php foreach( $posts as $post ) {
-			global $post;
-			setup_postdata( $post );
-			the_title(
-				sprintf( '<li class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-				'</a></li>'
-			);
+		<?php foreach( $links as $link ) {
+			printf( '<li class="entry-title">%s</li>', $link );
 		} ?>
 		</ul>
 	</section>
 <?php
-wp_reset_postdata();
 }
