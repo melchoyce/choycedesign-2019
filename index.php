e933ca5022a58c08b4fb53324fb728283e9e6b66
Kelly Dwan <ryelle@users.noreply.github.com>
2018-12-01 13:16:47 -0500

    Remove archive-specific template

--- index.php
+++ index.php
@@ -20,25 +20,39 @@ get_header(); ?>
 	if ( have_posts() ) :
 
 		if ( is_home() && ! is_front_page() ) : ?>
-			<header>
+			<header class="page-header">
 				<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 			</header>
 
 		<?php
-		endif;
+		elseif ( is_archive() ) : ?>
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
 
-		/* Start the Loop */
-		while ( have_posts() ) : the_post();
+		<?php
+		endif; ?>
 
-			/*
-				* Include the Post-Format-specific template for the content.
-				* If you want to override this in a child theme, then include a file
-				* called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				*/
-			get_template_part( 'template-parts/content', get_post_format() );
+		<div class="page-content">
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
 
-		endwhile;
+				/*
+					* Include the Post-Format-specific template for the content.
+					* If you want to override this in a child theme, then include a file
+					* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					*/
+				get_template_part( 'template-parts/content', get_post_format() );
 
+			endwhile; ?>
+
+		</div>
+
+		<?php
 		the_posts_navigation();
 
 	else :
