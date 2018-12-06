e7afc8c91f28d058af9227425d4408e688540262
Kelly Dwan <ryelle@users.noreply.github.com>
2018-12-05 21:19:23 -0500

    Update content width

--- functions.php
+++ functions.php
@@ -113,7 +113,7 @@ add_action( 'after_setup_theme', 'choycedesign_2019_setup' );
  * @global int $content_width
  */
 function choycedesign_2019_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'choycedesign_2019_content_width', 640 );
+	$GLOBALS['content_width'] = apply_filters( 'choycedesign_2019_content_width', 730 );
 }
 add_action( 'after_setup_theme', 'choycedesign_2019_content_width', 0 );
 
