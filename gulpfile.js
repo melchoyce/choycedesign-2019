32119242df2133265061814ef6f84075e2b70d82
Kelly Dwan <ryelle@users.noreply.github.com>
2019-08-17 14:43:32 -0400

    Update SVG sources

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var livereload = require('gulp-livereload');
+const path = require('path');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const postcssSVG = require('postcss-inline-svg')
+const livereload = require('gulp-livereload');
 
 const css = function() {
-	var processors = [autoprefixer({ browsers: ['last 2 versions'] })];
+	var processors = [
+		postcssSVG({
+			paths: [ path.resolve( './' ) ],
+		}),
+		autoprefixer(),
+	];
+
 	return gulp
 		.src('./scss/style.scss')
 		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
